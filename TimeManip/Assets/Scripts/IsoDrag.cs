3de6e2d0654a3431ca0581fb7abaad24daab732b
Felina-Lain <sawula@free.fr>
2016-11-29 17:28:55 +0100

    no message

--- TimeManip/Assets/Scripts/IsoDrag.cs
+++ TimeManip/Assets/Scripts/IsoDrag.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System;
 
 public class IsoDrag : MonoBehaviour {
 	
@@ -7,26 +9,33 @@ public class IsoDrag : MonoBehaviour {
 	Vector3 startPos;
 	int dragState = 0;
 
-	private Vector3 targetPosition;
-	private Vector3 startPosition;
 	private Vector3 mPos1;
 	private Vector3 mPosWorld1;
+
+	public Vector3[] touchPos = new Vector3[3];
+	[SerializeField]
+	private int touchTip;
 
 	public void Start()
 	{
 		dragState = 0;
+		touchTip = 0;
 
 	}
 
 	void Update()
 	{
+		// check if we have one touch
+		//if(Input.touchCount == 1)
+		//{
+		//	// get touch pos on this X-Y plane
+		//	mPos1 = Input.GetTouch(0).position;
 
-
-		// check if we have a mouse down
-		if(Input.touchCount == 1)
-		{
-			// get mouse pos on this X-Y plane
-			mPos1 = Input.GetTouch(0).position;
+		if(Input.GetMouseButton(0))
+     	{
+			
+     	// get touch pos on this X-Y plane
+			mPos1 = Input.mousePosition;
 			mPos1.z = Camera.main.transform.InverseTransformPoint(transform.position).z;
 			mPosWorld1 = Camera.main.ScreenToWorldPoint(mPos1);
 
@@ -43,12 +52,11 @@ public class IsoDrag : MonoBehaviour {
 		// drag the object if enabled
 		if(dragState == 1)
 		{
+			GetComponent<Rigidbody> ().isKinematic = true;
 			//turn off some scripts
-			if (this.name.Contains ("Cubepatternfixe")) {
+			if (this.name.Contains ("Cubepattern")) {
 				this.GetComponent<MoveToWaypoints> ().enabled = false;
 			}
-			//record position
-			startPosition= Input.GetTouch(0).position;
 			//move the object and pattern if needed
 			if(this.name.Contains("Cubepatternmove")){
 
@@ -59,27 +67,40 @@ public class IsoDrag : MonoBehaviour {
 			// move the object with mouse if normal
 			transform.position = new Vector3 (mPosWorld1.x - delta.x , transform.position.y , mPosWorld1.z - delta.z);
 
+
+			//store touch position for throw
+			touchPos[touchTip] = mPosWorld1;
+			touchTip++;
+			if (touchTip > 2) {
+				touchTip = 0;}
+
 		}
 			
 			
 
 
 		// end drag
-		if(dragState == 1 | dragState ==2 && Input.GetMouseButtonUp(0))
+		if(dragState == 1 && Input.GetMouseButtonUp(0))
 		{	
-			//turn the offed scripts
-			if (this.name.Contains ("Cubepatternfixe")) {
-				this.GetComponent<MoveToWaypoints> ().enabled = true;
-			}
-
 			//throw the item
-			//targetPosition = Input.GetTouch(0).position;
-			//Vector3 direction = (targetPosition - startPosition);
-			//direction.Normalize();
-			//GetComponent<Rigidbody>().AddForce(direction * 0.1f, ForceMode.Impulse);
+			if(!this.name.Contains ("fixe")) 
+			GetComponent<Rigidbody> ().isKinematic = false;
+
+			Vector3 direction = (touchPos[2] - touchPos[0]);
+			direction.Normalize();
+			GetComponent<Rigidbody>().AddForce(direction * 500f, ForceMode.Impulse);
 
 			//turn off drag
 			dragState = 0;
+
+			//turn the offed scripts
+			if (this.name.Contains ("Cubepattern")) {
+				this.GetComponent<MoveToWaypoints> ().enabled = true;
+				}
+			for ( int i = 0; i < touchPos.Length; i++)
+			{
+				touchPos[i] = new Vector3(0,0,0);
+			}
 		}
 	}
 
