f4e54203a8ced8bb497a11c5b26f894209337d61
Felina-Lain <sawula@free.fr>
2016-12-05 20:17:58 +0100

    Gyro a bit better

--- TimeManip/Assets/Scripts/GyroCam.cs
+++ TimeManip/Assets/Scripts/GyroCam.cs
@@ -35,33 +35,12 @@ public class GyroCam : MonoBehaviour {
 		float newPositionx = Mathf.SmoothDamp(transform.position.x, Input.gyro.rotationRateUnbiased.x, ref _velocity, damp_speed);
 		float newPositiony = Mathf.SmoothDamp(transform.position.y, Input.gyro.rotationRateUnbiased.y, ref _velocity, damp_speed);
 		transform.Rotate (newPositionx, 0, newPositiony);
+	
+	}
 
-		if (Input.touchCount > 0)
-		{
-			//If there's already been a tap within the timer, launch the move
-			if (_cooldown > 0 && _count == 1 /*Number of Taps you want Minus One*/)
-			{
+	public void ResetGyro () {
 
-				transform.rotation = initialRotation;
-
-
-			}
-			//Else launch timer
-			else
-			{
-				_cooldown = 0.5f;
-				_count += 1;
-			}
-		}
-		//If timer launched, count time
-		if (_cooldown > 0)
-		{
-			_cooldown -= Time.deltaTime;
-		}
-		else
-		{
-			_count = 0;
-		}
+		transform.rotation = initialRotation;
 	}
 		
 }
