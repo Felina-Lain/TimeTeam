d93712076dbdabb39c842d30f84b04b8a4f86caa
Felina-Lain <sawula@free.fr>
2016-12-09 12:42:45 +0100

    camera with tilt, need testing and calibrating

---
+++ TimeManip/Assets/Scripts/Camera/TiltCam.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TiltCam : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void FixedUpdate(){
+		//Vector3 movement = new Vector3 (Input.acceleration.x, 0.0f, 0.0f);
+		//GetComponent<Rigidbody>().velocity = movement * speed;
+
+		Vector3 dir = Vector3.zero;
+		dir.y = Input.acceleration.x;
+		dir.x = Input.acceleration.y;
+		//if (dir.sqrMagnitude > 1)
+		//	dir.Normalize();
+
+		dir *= Time.deltaTime * speed;
+
+		transform.Rotate (dir);
+	}
+
+}
