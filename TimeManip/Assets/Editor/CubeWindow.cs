d8bf07f600062d8f6eebf214682eea4e0b48d2e6
Felina-Lain <sawula@free.fr>
2017-01-06 10:49:06 +0100

    push with new graphisms need full 3d assets

--- TimeManip/Assets/Editor/CubeWindow.cs
+++ TimeManip/Assets/Editor/CubeWindow.cs
@@ -6,15 +6,20 @@ using System.Collections.Generic;
 public class CubeWindow : EditorWindow //Deriving from editor window to get window and GUI elements.
 {
 	List<bool> showGeneralSettings = new List<bool>();//create a list of foldouts, one for each objects
+
 	private CubeManager[] Cubemag; //array of target objects
+
+	Vector2 scrollPos; //declare the scrolling
+
 	[MenuItem("Window/Cube Window")] //Declaring to Unity engine that this is meant as a menu item.
+
 	public static void ShowWindow() //Telling unity what MenuItem - "WIndowCube" does.
 	{
 		EditorWindow cubeWindow = EditorWindow.GetWindow(typeof(CubeWindow)); //Getting our window for unity to use.
 		GUIContent titlecontent = new GUIContent("Cube Window"); //Creating some title content.
 		cubeWindow.titleContent = titlecontent; //Setting the title content to the window.
-		cubeWindow.maxSize = new Vector2(300, 500); //Declaring the max size of the window.
-		cubeWindow.minSize = new Vector2(300, 500); //Declaring the min size of the window.
+		//cubeWindow.maxSize = new Vector2(500, 500); //Declaring the max size of the window. Prevent resizing
+		//cubeWindow.minSize = new Vector2(500, 500); //Declaring the min size of the window. Prevent resizing
 		cubeWindow.ShowPopup(); //Showing our window as a Popup, I.E., a default editor style.
 	}
 
@@ -25,11 +30,15 @@ public class CubeWindow : EditorWindow //Deriving from editor window to get wind
 
 		GUILayout.Label("List of Cubes", EditorStyles.boldLabel); //Making a label in our vertical view, declaring its contents, and adding editor flare.
 
+		scrollPos = GUILayout.BeginScrollView(scrollPos,false,true);//call the scrolling
+
 		Cubemag = FindObjectsOfType<CubeManager>(); //Collecting our cube info, if too many cube, do this outside of GUI
+
 
 		for (int i = 0; i < Cubemag.Length; i++) //Your loop to display the objects, THIS must exist in OnGUI.
 		{
-			showGeneralSettings.Add (true); 
+			showGeneralSettings.Add (true); // for each each add a foldout in the foldout list
+
 			if (Cubemag[i] != null) //Preventing null errors if the objects are removed.
 			{
 				Cubemag[i] = (CubeManager)EditorGUILayout.ObjectField(Cubemag[i], typeof(CubeManager), true); //Declaring our object as an object field, with the type of object we want, and allowing scene object.
@@ -45,10 +54,10 @@ public class CubeWindow : EditorWindow //Deriving from editor window to get wind
 				EditorGUIUtility.fieldWidth = 10;//changing the lenght of the field space
 
 				Cubemag[i].lifeTime = EditorGUILayout.FloatField("Life Time", Cubemag[i].lifeTime); //the lifeTime property of cubemap can be assigned to a GUI element here.
-				EditorGUIUtility.fieldWidth = 0.01f;
+				EditorGUIUtility.fieldWidth = 0.01f;//changing the lenght of the field space
 
 				Cubemag[i].myGroup = (CubeGroups)EditorGUILayout.EnumPopup(Cubemag[i].myGroup);
-				EditorGUIUtility.labelWidth = 80;
+				EditorGUIUtility.labelWidth = 80;//changing the lenght of the field space
 
 				Cubemag[i].startCube = EditorGUILayout.Toggle("Start Cube", Cubemag[i].startCube);
 
@@ -56,6 +65,7 @@ public class CubeWindow : EditorWindow //Deriving from editor window to get wind
 				}
 			}
 		}
+		GUILayout.EndScrollView (); //close the scrolling
 		EditorGUILayout.EndVertical(); // And closing our last area.
 
 	}
--- TimeManip/Assets/Scripts/Camera/CamLock.cs
+++ TimeManip/Assets/Scripts/Camera/CamLock.cs
@@ -13,7 +13,7 @@ public class CamLock : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		transform.LookAt (_pivot, transform.up);
+		transform.LookAt (_pivot, Vector3.up);
 	
 	}
 }
